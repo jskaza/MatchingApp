3a998148f47d4074ccce32761e762764cb936631
jskaza <jonathan.skaza@gmail.com>
2022-07-06 16:19:31 -0400

    move routes -> app.jl

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,9 @@
 using Genie
 using Genie.Router
+using Genie.Requests
+using Genie.Renderer.Html
+using Genie.Renderer.Json
+using Match
 
 function launchServer(port)
 
@@ -9,8 +13,26 @@ function launchServer(port)
 
     println("port set to $(port)")
 
+    form = """
+    <form action="/" method="POST" enctype="multipart/form-data">
+    <input type="file" name="yourfile" /><br/>
+    <input type="submit" value="Submit" />
+    </form>
+    """
+
     route("/") do
-        "Hi there!"
+    html(form)
+    end
+
+    route("/", method = POST) do
+    if infilespayload(:yourfile)
+        write(filespayload(:yourfile))
+        types = [Float64, String, String, Float64, Float64,Float64, Float64, Float64]
+        X = ["sex","FL","RW","BD"]
+        Match.main(filename(filespayload(:yourfile)), types,"id","sp","B","O",X,1,true,false)
+    else
+        "No file uploaded"
+    end
     end
 
     Genie.AppServer.startup()
