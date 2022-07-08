bbeea1a4819350ff7c6b6ab6d05ebd3fb4e41319
jskaza <jonathan.skaza@gmail.com>
2022-07-08 13:53:21 -0400

    working version of app

--- public/js/select2.js
+++ public/js/select2.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
     $('.js-example-basic-multiple').select2();
 });
+
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
+// $.fn.serializeObject = function() {
+//     var o = {};
+//     var a = this.serializeArray();
+//     $.each(a, function() {
+//         if (o[this.name]) {
+//             if (!o[this.name].push) {
+//                 o[this.name] = [o[this.name]];
+//             }
+//             o[this.name].push(this.value || '');
+//         } else {
+//             o[this.name] = this.value || '';
+//         }
+//     });
+//     return o;
+// };
+
+// $(function() {
+//     $('form.match').on('submit', function(e) {
+//       e.preventDefault();
+
+//       var formData = $(this).serializeObject();
+//       console.log(formData);
+//       $('.datahere').html(formData);
+//     });
+// });
--- routes.jl
+++ routes.jl
@@ -8,56 +8,6 @@ using PrettyTables
 using CSV
 using DataFrames
 
-# <head>
-#   <meta charset="utf-8">
-#   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-#   <meta name="viewport" content="width=device-width, initial-scale=1">
-#   <title>Genie :: The highly productive Julia web framework</title>
-
-#   <!-- Bootstrap -->
-#   <link href="/css/genie/bootstrap.min.css" rel="stylesheet">
-#   <link href="/css/genie/style.css" rel="stylesheet">
-#   <link href="/css/genie/prism.css" rel="stylesheet" />
-# </head>
-
-# <div class="container-fluid">
-#     <!-- Start: Header -->
-#     <div class="row hero-header" id="home">
-#       <div class="col-md-7">
-#         <h1 id="main-heading">Welcome!</h1>
-#         <h3>It works! You have successfully created and started your Genie app. </h3>
-#       </div>
-#     </div>
-#     <!-- End: Header -->
-# </div>
-
-form = """
-<div>
-<form action="/" method="POST" enctype="multipart/form-data">
-  <input type="file" name="dataset" /><br/>
-  <input type="submit" value="Submit" />
-</form>
-"""
-
-form = """
-<div class="container-fluid">
-<script src="js/select2.js"></script>
-
-<form>
-<input type="checkbox" id=replacement" name="replacement" value="replacement">
-<label for="replacement">Replacement</label><br>
-
-<input type="checkbox" id=n_exact" name="n_exact" value="n_exact">
-<label for="n_exact">N Exact</label><br>
-
-<select class="js-example-basic-multiple" name="states[]" multiple="multiple"  style="width: 75%>
-  <option value="AL">Alabama</option>
-  <option value="WY">Wyoming</option>
-</select>
-</form> 
-</div>
-"""
-
 route("/") do 
   html(path"app/resources/file_form.jl.html", layout = path"app/layouts/app.jl.html")
 end
@@ -65,22 +15,40 @@ end
 route("/", method = POST) do
   if infilespayload(:dataset)
     write(filespayload(:dataset))
-    io = IOBuffer();
+
     df = CSV.read(filename(filespayload(:dataset)), DataFrame; delim = '\t', header = true)
 
-    pretty_table(io, describe(df), backend = :html)
-    tab = String(take!(io))
-    html(tab, layout = path"app/layouts/app.jl.html")
+    col_names = names(df)
+    html(path"app/resources/matching_form.jl.html", col_names = col_names, df=filespayload(:dataset), layout = path"app/layouts/app.jl.html")
+
+    # pretty_table(io, describe(df), backend = :html)
+    # tab = String(take!(io))
+    # html(tab, layout = path"app/layouts/app.jl.html")
     # for i in names(df)
     #   render(i)
     # end
     # X = ["sex","FL","RW","BD"]
-    # io = IOBuffer();
-   
 
   else
-    "No file uploaded"
-  end
+
+    write(filespayload(:dataset2))
+
+    df = CSV.read(filename(filespayload(:dataset2)), DataFrame; delim = '\t', header = true, stringtype=String)
+
+    form = postpayload()
+    id = form[:_id]
+    y = form[:target]
+    case = form[:case]
+    control = form[:control]
+    X = form[Symbol("covariates[]")]
+    n = parse(Int64, form[:ratio])
+    n_exact = haskey(form, "n_exact")
+    replacement = haskey(form, "replacement")
+    matching = Match.main(df,id,y,case,control,X,n,n_exact,replacement);
+    io = IOBuffer();
+    pretty_table(io, matching, nosubheader=true, backend=:html)
+    String(take!(io))
+  end 
 end
 
 
